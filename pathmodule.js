6544a324bf1639136b36b47deb48c6decc7bb695
Varshithvhegde <varshithvh@gmail.com>
2022-10-30 08:43:52 +0530

    new read'

---
+++ pathmodule.js
@@ -0,0 +1,6 @@
+const path = require('path');
+
+const a = path.basename('C:\\Users\\varsh.LAPTOP-IG9S62OJ\\Downloads\\Anxiety & Depression Detection.html');
+console.log(a);
+const a1  = path.extname(__filename);
+console.log(a1);
