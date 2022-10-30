f6069acea8d3984b515dc56bb1825ca624fb27e8
Varshithvhegde <varshithvh@gmail.com>
2022-10-30 08:57:49 +0530

    new read

--- fsmodule.js
+++ fsmodule.js
@@ -7,3 +7,13 @@ console.log("readFile");//It will print first because it is asynchronous
 const a = fs.readFileSync('file.txt');
 console.log(a.toString());
 console.log("readFileSync");//It will print second because it is synchronous
+
+// Writing file
+
+fs.writeFile('file2.txt','Hello World',()=>{
+    console.log("File written");
+});
+console.log("writeFile");//It will print first because it is asynchronous
+
+const a1 = fs.writeFileSync('file2.txt','Hello World new file');
+console.log("writeFileSync");//It will print second because it is synchronous
